c517aa2c219bb487a074ec5ceae8905a841b1b0a
psychochair <47328027+psychochair@users.noreply.github.com>
2020-02-28 14:56:24 -0500

    Added pre/post conditions

--- dictio_lab5_v5/src/dictioGUI/AddEditButtonListener.java
+++ dictio_lab5_v5/src/dictioGUI/AddEditButtonListener.java
@@ -11,6 +11,12 @@ public class AddEditButtonListener implements ActionListener {
         this.observer = observer;
     }
 
+    /**
+     * Méthode qui configure le résultat de l'appui du bouton ajouterEditer
+     * Precondition: appui du bouton
+     * Postcondition: Ajout d'un mot, ou modification de la desc d'un mot.
+     * @param actionEvent
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
